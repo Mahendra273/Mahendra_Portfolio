68323add67e7712d26668b35d250dcb2776cf18b
mahendra <mahidhoni2000@gmail.com>
2024-07-07 18:46:40 +0530

    skills and profile changes

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 export default function Skills() {
-  console.log(skillsData)
+ 
   return (
     <>
     <div className='skill_main_conatiner'>
